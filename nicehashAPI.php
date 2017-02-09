188447c274813ee4b7d53a21d08703b45d717358
KittyCatTech <kittycattech@tutanota.com>
2017-02-09 11:31:35 +0000

    Add files via upload

--- nicehashAPI.php
+++ nicehashAPI.php
@@ -91,7 +91,7 @@ class Nicehash {
       }
    }
 
-   public function setPrice($order, $location, $price = 0.01) {
+   public function setPrice($order, $location, $max_price, $price = 0.01) {
       if($order['price'] < $price){
          $args = array();
          $args['method'] = 'orders.set.price';
@@ -116,8 +116,55 @@ class Nicehash {
          $args['order'] = $order['id'];
          $result = $this->api_nicehash($args);
          if(isset($result['result']['error'])) return "Error: " . $result['result']['error'];
+
+         // cancel order and relist if still above max_price
+         if( substr($result['result']['success'], 0, 23 ) == "New order price set to:"  ) {
+            $return_price = substr($result['result']['success'], 23 );
+            if(floatval($return_price) > $max_price && intval($order['workers']) > 0) {
+               //print_r($order);
+               $order['price'] = $price;
+               echo $this->removeOrder($order, $location) . PHP_EOL;
+               return $this->createOrder($order, $location) . PHP_EOL;
+            }
+         }
+
          return $result['result']['success'];
       }
+   }
+
+   public function removeOrder($order, $location) {
+      $args = array();
+      $args['method'] = 'orders.remove';
+      $args['id'] = $this->publicKey;
+      $args['key'] = $this->privateKey;
+      $args['location'] = $location;
+      $args['algo'] = 22;   // 22 = CryptoNight
+      $args['order'] = $order['id'];
+      sleep(2);
+      $result = $this->api_nicehash($args);
+      if(isset($result['result']['error'])) { echo  "Error: " . $result['result']['error'] . PHP_EOL; return; };
+      return $result['result']['success'];
+   }
+
+   public function createOrder($order, $location) {
+      $args = array();
+      $args['method'] = 'orders.create';
+      $args['id'] = $this->publicKey;
+      $args['key'] = $this->privateKey;
+      $args['location'] = $location;
+      $args['algo'] = 22;   // 22 = CryptoNight
+      $args['amount'] = $order['btc_avail'];
+      $args['price'] = $order['price'];
+      $args['limit'] = $order['limit_speed'];
+      $args['pool_host'] = $order['pool_host'];
+      $args['pool_port'] = $order['pool_port'];
+      $args['pool_user'] = $order['pool_user'];
+      $args['pool_pass'] = $order['pool_pass'];
+      //print_r($order);
+      sleep(10); // extra time for order to cancel and balance to reset
+      $result = $this->api_nicehash($args);
+      if(isset($result['result']['error'])) { echo  "Error: " . $result['result']['error'] . PHP_EOL; return; };
+      return $result['result']['success'];
    }
 
 }
